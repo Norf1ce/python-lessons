b7341654801e413365a01ffaecb2d89924cbf13c
Norf1ce <n_lyubevych@school5kp.org.ua>
2024-02-03 16:50:32 +0200

    lessons04

---
+++ p04/main.py
@@ -0,0 +1,21 @@
+
+#print('Text: %d Number: %s' %('Text', 123))
+import os
+os.system("slc") # clear
+#print(20//7)
+#print(20%7)
+#x = 7
+#x *= 10
+#print(x)
+print(7 == 7)
+print(7 >= 7)
+print(7 > 7)
+
+x = int(input("Input number:"))
+answer = "Error"
+
+if x >= 5:
+    if x <= 10:
+        answer = "OK"
+    
+print(answer)
